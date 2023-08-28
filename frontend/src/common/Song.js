cac87ba1a9b4df41621a1f508cead66f492e15c9
ilaymor <ilaymor@protonmail.com>
2023-08-28 19:00:11 +0300

    wetwewtwet

--- frontend/src/common/Song.js
+++ frontend/src/common/Song.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import Button from './Button';
 import { useNavigate } from 'react-router-dom';
+import thumbsUpIcon from '../Images/thumbs-up-icon.png'
+import thumbsDownIcon from '../Images/thumbs-down-icon.png'
+import shareIcon from '../Images/share-icon.png'
+
 
 const Song = ({song, onLiked, onDisliked}) => {
     const [isLiked, setIsLiked] = useState(song.liked);
@@ -19,21 +23,29 @@ const Song = ({song, onLiked, onDisliked}) => {
 
   return (
     <div className='song'>
-      <div className='left'>
-        <h1>{song.Name} </h1>
-        <h3>{song.artist.Name}</h3>
-      </div>
-      <div className='middle'>
-        <h2>{song.album.Name}</h2>
+      <div className='top'>
+        <div className='left'>
+          <div className='album-image'>
+            <img class='albumimage' src='https://m.media-amazon.com/images/I/31wx3zcYTfL._UF1000,1000_QL80_.jpg' />
+          </div>
+          <div className='details'>
+            <h1>{song.Name} </h1>
+            <h3>{song.artist.Name}</h3>
+          </div>
+        </div>
+        <div className='middle'>
+          <h2>{song.album.Name}</h2>
+        </div>
+        <div className='right'>
+          3:43
+        </div>
       </div>
       <div className='bottom'>
         {isLiked ?
-          <div onClick={handleDislike}>([]V)</div>
-        : <div onClick={handleLike}>([]^) </div>
+          <span onClick={handleDislike}><img class='icon' src={thumbsDownIcon}/></span>
+        : <span onClick={handleLike}><img class='icon' src={thumbsUpIcon}/></span>
         }
-        <Button text="new post" onClick={()=>{
-                navigate(`/newpost?type=song&id=${song._id}`)
-        }} />
+        <span onClick={()=>{navigate(`/newpost?type=song&id=${song._id}`)}}><img class='icon' src={shareIcon}/></span>
       </div>
 
     </div>
