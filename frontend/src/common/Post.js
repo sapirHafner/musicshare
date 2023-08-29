ad77b5ba05972bd8407e44715ce478d9f6a8e2f2
ilaymor <ilaymor@protonmail.com>
2023-08-29 12:01:44 +0300

    wetwette

--- frontend/src/common/Post.js
+++ frontend/src/common/Post.js
@@ -2,18 +2,16 @@ import React from 'react'
 import { getMusicalEntityBoxComponent } from '../Common/Utilities'
 const Post = ({post}) => {
   const musicalEntityComponent = getMusicalEntityBoxComponent(post.MusicalEntity)
-
   return (
     <div>
         <br />
         {musicalEntityComponent}
-        <h5>{post.title}</h5>
-        <p>{post.content}</p>
+        <h5>{post.Title}</h5>
+        <p>{post.Content}</p>
         <br />
         ___________ <br />
-        |{user.FirstName} {user.LastName} | <br / >
+        |{post.User.FirstName} {post.User.LastName} | <br / >
         ___________ <br />
-
     </div>
   )
 }
