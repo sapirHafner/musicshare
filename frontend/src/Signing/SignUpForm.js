69bc8d734583b5ee181dc9e72a87106f5ed67f85
Sapir Hafner <sapiri2005@gmail.com>
2023-09-03 09:27:33 +0300

    new

--- frontend/src/Signing/SignUpForm.js
+++ frontend/src/Signing/SignUpForm.js
@@ -1,47 +1,75 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
     
 const SignUpForm = ({OnSignUp}) => { 
 
-    const onSubmit= (event) => {
+    const [formData, setFormData] = useState({
+        firstname: '',
+        lastname: '',
+        email: '',
+        username: '',
+        password: '',
+      });
+      const { firstname, lastname, email, username, password } = formData;
+
+      const onChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+      };
+
+      function isFormNotValid() {
+        return (!firstname || !lastname || !email || !username || !password);
+      }
+       
+      const onSubmit= (event) => {
         event.preventDefault()
         const profile = {
-            FirstName: event.target.firstname.value,
-            LastName: event.target.lastname.value,
-            Email: event.target.email.value
+            FirstName: firstname,
+            LastName: lastname,
+            Email: email,
         }
         const user = {
-            Username: event.target.username.value,
-            Password:event.target.password.value
+            Username: username,
+            Password: password,
         }
         OnSignUp(user, profile)
     }
+
     return (
         <form onSubmit={onSubmit} >
             <h2>Let's create a MusicShare profile!</h2>
             <br></br>
             <label htmlFor='firstname'> First Name: </label>
-            <input type='text' name='firstname'/>
+            <input type='text' name='firstname' value={firstname} onChange={onChange} />
             <br/>
             <label htmlFor='lastname'> Last Name: </label>
-            <input type='text' name='lastname'/>
+            <input type='text' name='lastname' value={lastname} onChange={onChange}/>
             <br/>
             <label htmlFor='email'> Mail: </label>
-            <input type='text' name='email'/>
+            <input type='email' name='email' value={email} onChange={onChange} />
             <br/>
             <label htmlFor='username'> User Name: </label>
-            <input type='text' name='username'/>
+            <input type='text' name='username' value={username} onChange={onChange}/>
+        
             <br/>
             <label htmlFor='password'> Password: </label>
-            <input type='password' name='password'/>
+            <input type='password' name='password' value={password} onChange={onChange}/>
+    
             <br/>
             <br/>
-            <input type='submit' value='Sign up!'/>
-            <br></br>
-            <br></br>
-            <Link to='/login'>Back to login page...</Link> 
-
+           
+            {isFormNotValid() ? (
+            <p>Don't be lazy! <br></br> Please fill all the boxes above..</p>
+    
+      ) : (
+        <input type='submit' value='Sign up!'/> 
+      )} 
+        <div> 
+         <br></br>
+        <Link to='/login'>Back to login page...</Link> </div> 
         </form>
     );
+
 }
 
 export default SignUpForm;
