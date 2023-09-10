8e3d23a83fc6de5e141a43ed83de86c31ddc0cc3
ilaymor <ilaymor@protonmail.com>
2023-09-10 13:38:04 +0300

    wetwet

--- README.md
+++
@@ -1,2 +0,0 @@
-# musicshare
-For 'Applications' course
--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@ cd frontend
 npm install &
 npm start &
 cd ../backend/mongodb/
-pwd
 mongod --dbpath ./data/db &
 cd ../server/
 npm install &
